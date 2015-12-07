723ebab1358bb050f20589a89bac5617fde52430
Michael Adrian <michaeladrian39@gmail.com>
2015-12-07 17:35:29 +0700

    added rules.php and global-rules.php

--- view/global-rules.php
+++ view/global-rules.php
@@ -14,6 +14,53 @@
             <li>
                 You will not post or request personal information ("dox") or calls to invasion ("raids"). Inciting or participating in cross-board (intra-4chan) raids is also not permitted.
             </li>
+            <li>
+              All boards that default to the Yotsuba B or Burichan (blue) themes are to be considered "work safe." Violators may be temporarily banned and their posts removed. Note: Spoilered pornography or other "not safe for work" content is NOT allowed on the work safe boards.
+            </li>
+            <li>
+              The quality of posts is extremely important to this community. Contributors are encouraged to provide high-quality images and informative comments.
+            </li>
+            <li>
+              Submitting false or misclassified reports, or otherwise abusing the reporting system may result in a ban. Replying to a thread stating that you've reported or "saged" it, or another post, is also not allowed.
+            </li>
+            <li>
+              Complaining about 4chan (its policies, moderation, etc) on the imageboards may result in post deletion and a ban.
+            </li>
+            <li>
+              Evading your ban will result in a permanent one. Instead, wait and appeal it!
+            </li>
+            <li>
+              No spamming or flooding of any kind. No intentionally evading spam or post filters.
+            </li>
+            <li>
+              Advertising (all forms) is not welcome—this includes any type of referral linking, "offers", soliciting, begging, stream threads, etc.
+            </li>
+            <li>
+              Impersonating a 4chan administrator, moderator, or janitor is strictly forbidden.
+            </li>
+            <li>
+              Do not use avatars or attach signatures to your posts.
+            </li>
+            <li>
+              The use of scrapers, bots, or other automated posting or downloading scripts is prohibited. Users may also not post from proxies, VPNs, or Tor exit nodes.
+            </li>
+            <li>
+              All pony/brony threads, images, Flashes, and avatars belong on /mlp/.
+            </li>
+            <li>
+              All request threads belong on /r/ unless otherwise noted.
+            </li>
+            <li>
+              Do not upload images containing additional data such as embedded sounds, documents, archives, etc.
+            </li>
         </ol>
+        <br>
+        <p>
+          Global rules apply to all boards unless otherwise noted.
+        </p>
+        <br>
+        <p>
+          Remember: The use of 4chan is a privilege, not a right. The 4chan moderation team reserves the right to revoke access and remove content for any reason without notice.
+        </p>
     </div>
 </div>
--- view/rules.php
+++ view/rules.php
@@ -1,8 +1,8 @@
 <div>
     <h1 class="title">Rules</h1>
     <div class="common-box">
-        By using this website (the "site"), you agree that you'll follow these rules, and 
-        understand that if we reasonably think you haven't followed these rules, 
+        By using this website (the "site"), you agree that you'll follow these rules, and
+        understand that if we reasonably think you haven't followed these rules,
         we may (at our own discretion) terminate your access to the site:
     </div>
     <div class="common-box">
@@ -17,6 +17,67 @@
                 <div><a href="image-boards#adultcartoons" class="rules-item">Adult Cartoons</a></div>
                 <div><a href="image-boards#advice" class="rules-item">Advice</a></div>
                 <div><a href="image-boards#animalandnature" class="rules-item">Animal & Nature</a></div>
+                <div><a href="image-boards#alternativesports" class="rules-item">Alternative Sports</a></div>
+                <div><a href="image-boards#random" class="rules-item">Random</a></div>
+                <div><a href="image-boards#businessandfinance" class="rules-item">Business & Finance</a></div>
+                <div><a href="image-boards#cute" class="rules-item">Cute</a></div>
+                <div><a href="image-boards#cosplayandegl" class="rules-item">Cosplay & EGL</a></div>
+                <div><a href="image-boards#foodandcooking" class="rules-item">Food & Cooking</a></div>
+                <div><a href="image-boards#cutemale" class="rules-item">Cute/Male</a></div>
+                <div><a href="image-boards#comicsandcartoons" class="rules-item">Comics & Cartoons</a></div>
+                <div><a href="image-boards#hentaialternative" class="rules-item">Hentai/Alternative</a></div>
+                <div><a href="image-boards#doityourself" class="rules-item">Do It Yourself</a></div>
+                <div><a href="image-boards#ecchi" class="rules-item">Ecchi</a></div>
+                <div><a href="image-boards#fashion" class="rules-item">Fashion</a></div>
+                <div><a href="image-boards#fitness" class="rules-item">Fitness</a></div>
+                <div><a href="image-boards#technology" class="rules-item">Technology</a></div>
+                <div><a href="image-boards#graphicdesign" class="rules-item">Graphic Design</a></div>
+                <div><a href="image-boards#adultgif" class="rules-item">Adult GIF</a></div>
+                <div><a href="image-boards#hentai" class="rules-item">Hentai</a></div>
+                <div><a href="image-boards#hardcore" class="rules-item">Hardcore</a></div>
+                <div><a href="image-boards#historyandhumanities" class="rules-item">History & Humanities</a></div>
+                <div><a href="image-boards#handsomemen" class="rules-item">Handsome Men</a></div>
+                <div><a href="image-boards#highresolution" class="rules-item">High Resolution</a></div>
+                <div><a href="image-boards#oekaki" class="rules-item">Oekaki</a></div>
+                <div><a href="image-boards#artworkcritique" class="rules-item">Artwork/Critique</a></div>
+                <div><a href="image-boards#international" class="rules-item">International</a></div>
+                <div><a href="image-boards#otakuculture" class="rules-item">Otaku Culture</a></div>
+                <div><a href="image-boards#weapons" class="rules-item">Weapons</a></div>
+                <div><a href="image-boards#literature" class="rules-item">Literature</a></div>
+                <div><a href="image-boards#lgbt" class="rules-item">LGBT</a></div>
+                <div><a href="image-boards#mecha" class="rules-item">Mecha</a></div>
+                <div><a href="image-boards#pony" class="rules-item">Pony</a></div>
+                <div><a href="image-boards#music" class="rules-item">Music</a></div>
+                <div><a href="image-boards#transportation" class="rules-item">Transportation</a></div>
+                <div><a href="image-boards#currentnews" class="rules-item">Current News</a></div>
+                <div><a href="image-boards#auto" class="rules-item">Auto</a></div>
+                <div><a href="image-boards#outdoor" class="rules-item">Outdoors</a></div>
+                <div><a href="image-boards#photography" class="rules-item">Photography</a></div>
+                <div><a href="image-boards#papercraftandorigami" class="rules-item">Papercraft & Origami</a></div>
+                <div><a href="image-boards#politicallyincorrect" class="rules-item">Politically Incorrect</a></div>
+                <div><a href="image-boards#request" class="rules-item">Request</a></div>
+                <div><a href="image-boards#robot9001" class="rules-item">ROBOT9001</a></div>
+                <div><a href="image-boards#shit4chansays" class="rules-item">Shit 4chan Says</a></div>
+                <div><a href="image-boards#sexybeautifulwomen" class="rules-item">Sexy Beautiful Women</a></div>
+                <div><a href="image-boards#scienceandmath" class="rules-item">Science & Math</a></div>
+                <div><a href="image-boards#camsandmeetups" class="rules-item">Cams & Meetups</a></div>
+                <div><a href="image-boards#sports" class="rules-item">Sports</a></div>
+                <div><a href="image-boards#torrents" class="rules-item">Torrents</a></div>
+                <div><a href="image-boards#traditionalgames" class="rules-item">Traditional Games</a></div>
+                <div><a href="image-boards#toys" class="rules-item">Toys</a></div>
+                <div><a href="image-boards#trash" class="rules-item">Trash</a></div>
+                <div><a href="image-boards#travel" class="rules-item">Travel</a></div>
+                <div><a href="image-boards#televisionandfilm" class="rules-item">Television & Film</a></div>
+                <div><a href="image-boards#yuri" class="rules-item">Yuri</a></div>
+                <div><a href="image-boards#videogames" class="rules-item">Video Games</a></div>
+                <div><a href="image-boards#videogamegenerals" class="rules-item">Video Game Generals</a></div>
+                <div><a href="image-boards#pokemon" class="rules-item">Pokémon</a></div>
+                <div><a href="image-boards#retrogames" class="rules-item">Retro Games</a></div>
+                <div><a href="image-boards#animewallpapers" class="rules-item">Anime/Wallpapers</a></div>
+                <div><a href="image-boards#wallpapersgeneral" class="rules-item">Wallpapers/General</a></div>
+                <div><a href="image-boards#worksafegif" class="rules-item">Worksafe GIF</a></div>
+                <div><a href="image-boards#yaoi" class="rules-item">Yaoi</a></div>
+                <div><a href="image-boards#paranormal" class="rules-item">Paranormal</a></div>
             </div>
             <div class="col-md-3">
                 <div><a class="rules-title-link" href="upload-boards">Upload Boards</a></div>
